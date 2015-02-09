ffb358ba6d352f426720fc45435e33c7fe08249b
Travis Pew <travis@travispew.com>
2015-02-09 18:46:00 -0500

    Crop circles

--- textbook_work/travis_pew/Hypnosister/Hypnosister/HypnosisView.m
+++ textbook_work/travis_pew/Hypnosister/Hypnosister/HypnosisView.m
@@ -30,18 +30,18 @@
     center.x = bounds.origin.x + bounds.size.width / 2.0;
     center.y = bounds.origin.y + bounds.size.height / 2.0;
     
-    // The circle will be the largest that will fit in the view
-    float radius = MIN(bounds.size.width, bounds.size.height) / 2.0;
+    // The largest circle will circumscribe the view
+    float maxRadius = hypot(bounds.size.width, bounds.size.height) / 2.0;
     
     UIBezierPath *path = [[UIBezierPath alloc] init];
     
-    // Add an arc to the path at the center, with radius of radius,
-    // from 0 to 2*PI radians (a circle)
-    [path addArcWithCenter:center
-                    radius:radius
-                startAngle:0.0
-                  endAngle:M_PI * 2.0
-                 clockwise:YES];
+    for (float currentRadius = maxRadius; currentRadius > 0; currentRadius -= 20) {
+        [path addArcWithCenter:center
+                        radius:currentRadius
+                    startAngle:0.0
+                      endAngle:M_PI * 2.0
+                     clockwise:YES];
+    }
     
     // Configure line width to 10 points
     path.lineWidth = 10;
