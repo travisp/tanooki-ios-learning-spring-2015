0f220df2fc27dea8c31b3d85f144dfdfa2514d07
Travis Pew <travis@travispew.com>
2015-02-09 18:37:53 -0500

    draw circle inside drawRect

--- textbook_work/travis_pew/Hypnosister/Hypnosister/HypnosisView.m
+++ textbook_work/travis_pew/Hypnosister/Hypnosister/HypnosisView.m
@@ -10,13 +10,33 @@
 
 @implementation HypnosisView
 
-/*
+
 // Only override drawRect: if you perform custom drawing.
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect {
-    // Drawing code
+    CGRect bounds = self.bounds;
+    
+    // Figure out the center of the bounds rectangle
+    CGPoint center;
+    center.x = bounds.origin.x + bounds.size.width / 2.0;
+    center.y = bounds.origin.y + bounds.size.height / 2.0;
+    
+    // The circle will be the largest that will fit in the view
+    float radius = MIN(bounds.size.width, bounds.size.height) / 2.0;
+    
+    UIBezierPath *path = [[UIBezierPath alloc] init];
+    
+    // Add an arc to the path at the center, with radius of radius,
+    // from 0 to 2*PI radians (a circle)
+    [path addArcWithCenter:center
+                    radius:radius
+                startAngle:0.0
+                  endAngle:M_PI * 2.0
+                 clockwise:YES];
+    
+    // Draw the line!
+    [path stroke];
 }
-*/
 
 
 @end
