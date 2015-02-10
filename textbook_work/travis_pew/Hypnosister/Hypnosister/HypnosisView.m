bed5050b2b68db5cff51644608a5ea002f43bc3a
Travis Pew <travis@travispew.com>
2015-02-09 19:42:01 -0500

    Complete Gold Challenge

--- textbook_work/travis_pew/Hypnosister/Hypnosister/HypnosisView.m
+++ textbook_work/travis_pew/Hypnosister/Hypnosister/HypnosisView.m
@@ -61,21 +61,56 @@
                                  center.y - (logoImage.size.height / 4.0),
                                  logoImage.size.width / 2.0,
                                  logoImage.size.height / 2.0);
-    
 
+    
+    CGFloat locations[2] = { 0.0, 1.0 };
+    CGFloat components[8] = { 0.0, 1.0, 0.0, 1.0,   // Start color is green
+                              1.0, 1.0, 0.0, 1.0 }; // End color is yellow
+    CGColorSpaceRef colorspace = CGColorSpaceCreateDeviceRGB();
+    CGGradientRef gradient = CGGradientCreateWithColorComponents(colorspace,
+                                                                 components,
+                                                                 locations,
+                                                                2);
+    CGPoint logoTopCenter = CGPointMake(logoRect.origin.x + (logoRect.size.width / 2.0),
+                                     logoRect.origin.y);
+    
+    CGPoint logoBottomLeft = CGPointMake(logoRect.origin.x, logoRect.origin.y + logoRect.size.height);
+    
+    CGPoint logoBottomRight = CGPointMake(logoRect.origin.x + logoRect.size.width, logoRect.origin.y + logoRect.size.height);
+  
+    CGPoint logoBottomCenter = CGPointMake(logoTopCenter.x,
+                                   logoRect.origin.y + logoRect.size.height);
+    
+    UIBezierPath *bezierTriangle = [UIBezierPath bezierPath];
+    
+    [bezierTriangle moveToPoint:logoTopCenter];
+    [bezierTriangle addLineToPoint:logoBottomLeft];
+    [bezierTriangle addLineToPoint:logoBottomRight];
+    [bezierTriangle closePath];
+    
+    CGContextSaveGState(currentContext);
+    [bezierTriangle addClip];
+    CGContextDrawLinearGradient(currentContext,
+                                gradient,
+                                logoTopCenter,
+                                logoBottomCenter,
+                                0);
+    CGContextRestoreGState(currentContext);
+ 
+    CGGradientRelease(gradient);
+    CGColorSpaceRelease(colorspace);
+    
     
     
     CGContextSaveGState(currentContext);
     
     CGContextSetShadow(currentContext, CGSizeMake(4,7), 3);
-
+    
     // Draw stuff here, it will appear with a shadow
     [logoImage drawInRect:logoRect];
     
     CGContextRestoreGState(currentContext);
     
-    // Draw stuff here, it will appear with no shadow
-
 }
 
 
