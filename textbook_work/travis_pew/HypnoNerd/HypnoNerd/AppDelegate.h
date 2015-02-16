9c53d82222d34a5929d6680e418adc29edf89a89
Travis Pew <travis@travispew.com>
2015-02-16 11:19:01 -0500

    Blank HypnoNerd project

---
+++ textbook_work/travis_pew/HypnoNerd/HypnoNerd/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HypnoNerd
+//
+//  Created by Travis Pew on 2/16/15.
+//  Copyright (c) 2015 Travis Pew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
