9ecd3c8c5092b5d1f6c91487053f09f1070386d1
Travis Pew <travis@travispew.com>
2015-02-16 11:15:25 -0500

    Panning between two views

--- textbook_work/travis_pew/Hypnosister/Hypnosister/AppDelegate.m
+++ textbook_work/travis_pew/Hypnosister/Hypnosister/AppDelegate.m
@@ -24,15 +24,19 @@
     CGRect screenRect = self.window.bounds;
     CGRect bigRect = screenRect;
     bigRect.size.width *= 2.0;
-    bigRect.size.height *= 2.0;
     
     // Create a screen-sized scroll view and add it to the window
     UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:screenRect];
     [self.window addSubview:scrollView];
     
-    // Create a super-sized hypnosis view and add it to the scroll view
-    HypnosisView *hypnosisView = [[HypnosisView alloc] initWithFrame:bigRect];
+    // Create a screen-sized hypnosis view and add it to the scroll view
+    HypnosisView *hypnosisView = [[HypnosisView alloc] initWithFrame:screenRect];
     [scrollView addSubview:hypnosisView];
+    
+    // Add a second screen-sized hypnosis view just off screen to the right
+    screenRect.origin.x += screenRect.size.width;
+    HypnosisView *anotherView = [[HypnosisView alloc] initWithFrame:screenRect];
+    [scrollView addSubview:anotherView];
     
     // Tell the scrol view how big its content area is
     scrollView.contentSize = bigRect.size;
