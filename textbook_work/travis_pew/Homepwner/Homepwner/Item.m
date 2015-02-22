3527ce27aa3f20674a5b45af4ad969f5c339f9e5
Travis Pew <travis@travispew.com>
2015-02-22 15:14:24 -0500

    Add in Item.h and Item.m from RandomItems

---
+++ textbook_work/travis_pew/Homepwner/Homepwner/Item.m
@@ -0,0 +1,78 @@
+//
+//  Item.m
+//  RandomItems
+//
+//  Created by Travis Pew on 2/2/15.
+//  Copyright (c) 2015 Travis Pew. All rights reserved.
+//
+
+#import "Item.h"
+
+@implementation Item
+
++ (instancetype)randomItem
+{
+    NSArray *randomAdjectiveList = @[@"Fluffy", @"Rusty", @"Shiny"];
+    NSArray *randomNounList = @[@" Bear", @" Spork", @" Mac"];
+    
+    NSInteger adjectiveIndex = arc4random() % [randomAdjectiveList count];
+    NSInteger nounIndex = arc4random() % [randomNounList count];
+    
+    NSString *randomName = [NSString stringWithFormat:@"%@ %@",
+                            randomAdjectiveList[adjectiveIndex],
+                            randomNounList[nounIndex]];
+    
+    int randomValue = arc4random() % 100;
+    
+    NSString *randomSerialNumber = [NSString stringWithFormat:@"%c%c%c%c%c", '0' + arc4random() % 10, 'A' + arc4random() % 26, '0' + arc4random() % 10, 'A' + arc4random() % 26, '0' + arc4random() % 10];
+    
+    Item *newItem = [[self alloc] initWithItemName:randomName
+                                    valueInDollars:randomValue
+                                      serialNumber:randomSerialNumber];
+    
+    return newItem;
+}
+
+- (instancetype)initWithItemName:(NSString *)name valueInDollars:(int)value serialNumber:(NSString *)sNumber
+{
+    self = [super init];
+    
+    if (self) {
+        _itemName = name;
+        _serialNumber = sNumber;
+        _valueInDollars = value;
+        _dateCreated = [[NSDate alloc] init];
+    }
+    
+    return self;
+}
+
+- (instancetype)initWithItemName:(NSString *)name
+{
+    return [self initWithItemName:name
+                   valueInDollars:0
+                     serialNumber:@""];
+}
+
+- (instancetype)init
+{
+    return [self initWithItemName:@"Item"];
+}
+
+- (void) dealloc
+{
+    NSLog(@" Destroyed: %@", self);
+}
+
+- (NSString *)description
+{
+    NSString *descriptionString =
+    [[NSString alloc] initWithFormat:@"%@ (%@): Worth $%d, recorded on %@",
+     self.itemName,
+     self.serialNumber,
+     self.valueInDollars,
+     self.dateCreated];
+    
+    return descriptionString;
+}
+@end
