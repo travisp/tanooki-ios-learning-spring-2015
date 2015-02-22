ddb19954a4135cabb4950e720b889361f61c632e
Travis Pew <travis@travispew.com>
2015-02-22 13:36:56 -0500

    Revert "Silver Challenge: UISegmentedControl"
    This reverts commit b0eae82274f8c4a3e13c2519eb989ea24b8d6573.

--- textbook_work/travis_pew/HypnoNerd/HypnoNerd/HypnosisView.h
+++ textbook_work/travis_pew/HypnoNerd/HypnoNerd/HypnosisView.h
@@ -10,6 +10,4 @@
 
 @interface HypnosisView : UIView
 
-@property (strong, nonatomic) UIColor *circleColor;
-
 @end
--- textbook_work/travis_pew/HypnoNerd/HypnosisViewController.m
+++ textbook_work/travis_pew/HypnoNerd/HypnosisViewController.m
@@ -9,10 +9,6 @@
 #import "HypnosisViewController.h"
 #import "HypnosisView.h"
 
-@interface HypnosisViewController()
-@property(nonatomic, weak) HypnosisView *viewReference;
-@end
-
 @implementation HypnosisViewController
 
 - (instancetype)initWithNibName:(NSString *)nibNameOrNil
@@ -44,47 +40,14 @@
     NSLog(@"HypnosisViewController loaded its view");
 }
 
--(void) colorSelected:(id)sender
-{
-     UISegmentedControl *sc = (UISegmentedControl *)sender;
-    
-    NSString *selectedString = [sc titleForSegmentAtIndex: [sc selectedSegmentIndex]];
-    
-    UIColor *color;
-    
-    if( [selectedString isEqualToString:@"Red"] ){
-        color = [UIColor redColor];
-    }else if ( [selectedString isEqualToString:@"Green"] ){
-        color = [UIColor greenColor];
-    }else if ( [selectedString isEqualToString:@"Blue"] ){
-        color = [UIColor blueColor];
-    }
-    
-    self.viewReference.circleColor = color;
-}
-
 - (void)loadView
 {
     // Create a view
     CGRect frame = [UIScreen mainScreen].bounds;
-    NSArray *items = @[@"Red", @"Green", @"Blue"];
-    UISegmentedControl *segmented = [[UISegmentedControl alloc] initWithItems:items];
-    CGRect sframe = segmented.frame;
-    sframe.origin.y = 100;
-    sframe.origin.x = (frame.size.width - sframe.size.width) / 2;
-    segmented.frame = sframe;
-    
-    
-    [segmented addTarget:self
-                  action:@selector(colorSelected:)
-        forControlEvents:UIControlEventValueChanged];
-    
     HypnosisView *backgroundView = [[HypnosisView alloc] initWithFrame:frame];
-    [backgroundView addSubview:segmented];
     
     // Set it as *the* view of this view controller
-    self.view = backgroundView;
-    self.viewReference = backgroundView;
+    self.view = backgroundView;    
 }
 
 @end
