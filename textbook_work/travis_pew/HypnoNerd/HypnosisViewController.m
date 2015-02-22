1ccb3b2f3670b827aa11b1814e081ec7c4b5f185
Travis Pew <travis@travispew.com>
2015-02-22 14:11:32 -0500

    interpolating motion effect

--- textbook_work/travis_pew/HypnoNerd/HypnosisViewController.m
+++ textbook_work/travis_pew/HypnoNerd/HypnosisViewController.m
@@ -72,6 +72,21 @@
         
         // Add the label to the hierarchy
         [self.view addSubview:messageLabel];
+        
+        UIInterpolatingMotionEffect *motionEffect;
+        motionEffect = [[UIInterpolatingMotionEffect alloc]
+                        initWithKeyPath:@"center.x"
+                        type:UIInterpolatingMotionEffectTypeTiltAlongHorizontalAxis];
+        motionEffect.minimumRelativeValue = @-25;
+        motionEffect.maximumRelativeValue = @25;
+        [messageLabel addMotionEffect:motionEffect];
+        
+        motionEffect = [[UIInterpolatingMotionEffect alloc]
+                        initWithKeyPath:@"center.y"
+                        type:UIInterpolatingMotionEffectTypeTiltAlongVerticalAxis];
+        motionEffect.minimumRelativeValue = @-25;
+        motionEffect.maximumRelativeValue = @25;
+        [messageLabel addMotionEffect:motionEffect];
     }
 }
 
